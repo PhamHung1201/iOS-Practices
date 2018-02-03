f8decf0c780e629f7ca00e151cbbc32e3d8f4490
Hung Pham <hung.pv@seesaa.vn>
2018-02-03 11:05:33 +0700

    Initial commit

---
+++ iOS PracticesTests/iOS_PracticesTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_PracticesTests.swift
+//  iOS PracticesTests
+//
+//  Created by Hung Pham Van on 2/3/18.
+//  Copyright © 2018 Hung Pham Van. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Practices
+
+class iOS_PracticesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
